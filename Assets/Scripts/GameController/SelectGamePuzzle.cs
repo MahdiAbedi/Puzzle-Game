832aa433630e8de6bcebd7e1bfefa168a91df89f
MahdiAbedi <mahdiabedi220@yahoo.com>
2018-01-09 16:07:06 +0330

    Initial commit

---
+++ Assets/Scripts/GameController/SelectGamePuzzle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectGamePuzzle : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject gameButtonPanel,levelMagegerPanel;
+
+	[SerializeField]
+	private Animator gameButtonPannleAnim,levelManagerAnim;
+
+
+	public string puzzleName;
+
+	public void SellectPuzzle(){
+	
+		puzzleName = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+		Debug.Log (puzzleName);
+		StartCoroutine(ShowLevels ());
+	}
+
+
+	IEnumerator ShowLevels(){
+		levelMagegerPanel.SetActive (true);
+		gameButtonPannleAnim.Play("FadeOut");
+		yield return new WaitForSeconds (0.7f);
+		gameButtonPanel.SetActive (false);
+		levelManagerAnim.Play("FadeIn");
+	}
+
+	public void BackToMenuBtn(){
+		StartCoroutine (BackToMenu());
+	}
+	IEnumerator BackToMenu(){
+		levelManagerAnim.Play ("FadeOut");
+		yield return new WaitForSeconds (0.7f);
+		levelMagegerPanel.SetActive (false);
+
+		gameButtonPanel.SetActive (true);
+		gameButtonPannleAnim.Play ("FadeIn");
+
+	}
+}
---
+++ Assets/Scripts/GameController/SettingPanel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingPanel : MonoBehaviour {
+
+	public GameObject settingPanel;
+	public Animator settingPanelAnim;
+
+
+	public void FadeInSetting(){
+		settingPanel.SetActive (true);
+		settingPanelAnim.Play ("FadeIn");
+	}
+
+
+
+	public void FadeOutSetting(){
+		StartCoroutine (CloseSettingPanel());
+	}
+
+	IEnumerator CloseSettingPanel(){
+		settingPanelAnim.Play ("FadeOut");
+		yield return new WaitForSeconds (0.7f);
+		settingPanel.SetActive (false);
+
+	}
+}
